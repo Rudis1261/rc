f092ca4fd85a561c19add24398d83fda50cec282
Rudi Strydom <iam@thatguy.co.za>
2014-12-11 08:57:59 +0200

    Updates

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-rc
-==
+#RC
+Tamiya RC car control, using **PIGPIO**
 
-RC car control, using PIGPIO
+###Objective
+
+I need a software solution to be able to control my RC Car via an interface and the Raspbery Pi. I will be communicating with the PI via bluetooth, and sending as little commands as possible. The idea was to submit 2 different types of commands, the first being a service request (for lights etc), and the second being a control request (steering and throttle)
+
+I am very bad at documenting things, so this is my effort to share my findings for the benefit of others.
+
+###What you need to get this to work yourself
+
+* This makes use of the awesome C Library **PIGPIO**: http://abyz.co.uk/rpi/pigpio/
+* For installation, follow his **make**, **make install** instructions found here: http://abyz.co.uk/rpi/pigpio/download.html
+
+###Initial Findings
+
+PIGPIO uses DMA to manage the pulse width modulation, now I am no expert on the subject so I won't make out to be one. But from what I understand it is as close to hardware PWM as you will get on a Raspberry Pi.
+
+I haven't played with it allot, but it seems very promising. Making it much easier to control pulse width modulation than with the vanilla RPi.GPIO pulse width modulation. So far I have been able to get the RC car's steering servo to behave much less sporadically than it was before with the RPi.GPIO.
+
+I was able to also create a light pulsing action. I plan to have a couple of lighting modes.
+* Pulsing break lights
+* Dim break lights, which light up when breaking
+* Strobing headlights, in unison as well as independently
+* Strobing all round the car. In patterns, as well as randomly
--- testy.py
+++ testy.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
-
-#*** WARNING ************************************************
-#*                                                          *
-#* All the tests make extensive use of gpio 4 (pin P1-7).   *
-#* Ensure that either nothing or just a LED is connected to *
-#* gpio 4 before running any of the tests.                  *
-#*                                                          *
-#* Some tests are statistical in nature and so may on       *
-#* occasion fail.  Repeated failures on the same test or    *
-#* many failures in a group of tests indicate a problem.    *
-#************************************************************
-
 import sys
 import time
 import struct
-
 import pigpio
+
 
 def pulse(PIN, TIME, LENGHT):
     pulseRange = 255
@@ -26,61 +14,72 @@ def pulse(PIN, TIME, LENGHT):
     for i in range(1, LENGHT):
 	if direction == "asc":
 	    direction = "desc"
-	else: 
+	else:
 	    direction = "asc"
 
 	for x in range(1, LENGHT):
 
 	    if direction == "asc":
-		 
 	        value = x * (pulseRange / LENGHT)
-	    else: 
+	    else:
 	        value = (LENGHT - x) * (pulseRange / LENGHT)
 	    print("Running " + str(value))
 	    pi.set_PWM_dutycycle(PIN, value)
-	    time.sleep(TIME)	
+	    time.sleep(TIME)
 
 
 
-def test_my_servo(SERVO, MIN=850, CENTER=1350, MAX=1750):
+def testServo(SERVO, MIN=850, CENTER=1350, MAX=1750):
+    print("Centerig Steering Servo")
     pi.set_servo_pulsewidth(SERVO, CENTER)
-    time.sleep(3)
- 
-    for i in range(CENTER, MIN, -20):
-	pi.set_servo_pulsewidth(SERVO, i)
-	time.sleep(0.1) 
 
     time.sleep(3)
+    print("To the left, to the left")
+
+    for i in range(CENTER, MIN, -20):
+	    pi.set_servo_pulsewidth(SERVO, i)
+	    time.sleep(0.1)
+
+    print("Hoorah")
+    time.sleep(3)
+    print("Bring it back now")
 
     for i in range(MIN, CENTER, 20):
-	pi.set_servo_pulsewidth(SERVO, i)
-	time.sleep(0.01)
+	    pi.set_servo_pulsewidth(SERVO, i)
+	    time.sleep(0.01)
 
+    print("Hoorah")
     time.sleep(3)
+    print("Centerig Steering Servo")
 
     for i in range (CENTER, MAX, 20):
-	pi.set_servo_pulsewidth(SERVO, i)
-	time.sleep(0.1)
+	    pi.set_servo_pulsewidth(SERVO, i)
+	    time.sleep(0.1)
 
+    print("Hoorah")
     time.sleep(3)
+    print("Centerig Steering Servo")
 
     pi.set_servo_pulsewidth(SERVO, CENTER)
     time.sleep(0.5)
     pi.set_servo_pulsewidth(SERVO, 0)
+    print("Servo test complete")
 
-GPIO=4
-SERVO=7
-pi = pigpio.pi()
+
+# Start the actual program
+GPIO  = 4
+SERVO = 7
+pi    = pigpio.pi()
 
 print("Testing Servo")
-test_my_servo(SERVO)
+testServo(SERVO)
 time.sleep(3)
 
-print("Pulsing")
-time.sleep(3)
 print("Pulsing")
 pulse(GPIO, 0.05, 30)
 print("All done")
-pi.write(4, 0)
-pi.stop()
 
+pi.write(4, 0)
+
+# Clear everything up and stop using the GPIO
+pi.stop()
