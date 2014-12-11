c751e7c7642ffddff869dee3a3b4c98667b3f5d4
Rudi Strydom <iam@thatguy.co.za>
2014-12-11 09:15:52 +0200

    Updates

--- testy.py
+++ testy.py
@@ -5,64 +5,68 @@ import struct
 import pigpio
 
 
-def pulse(PIN, TIME, LENGHT):
+def testPulse(PIN, TIME=0.05, LENGHT=30, INTERVAL=1):
+
+    print("Commencing Pulse")
     pulseRange = 255
     pi.set_PWM_range(PIN, pulseRange)
     pi.set_PWM_frequency(GPIO, 100)
     direction = "asc"
 
-    for i in range(1, LENGHT):
-	if direction == "asc":
-	    direction = "desc"
-	else:
-	    direction = "asc"
+    for i in range(1, LENGHT, INTERVAL):
+    	if direction == "asc":
+            for x in range(1, LENGHT, INTERVAL):
+                print("Running " + str(value))
+                pi.set_PWM_dutycycle(PIN, value)
+                time.sleep(TIME)
+    	    direction = "desc"
+    	else:
+            for x in range(LENGHT - 1, 2, -INTERVAL):
+                print("Running " + str(value))
+                pi.set_PWM_dutycycle(PIN, value)
+                time.sleep(TIME)
+    	    direction = "asc"
 
-	for x in range(1, LENGHT):
-
-	    if direction == "asc":
-	        value = x * (pulseRange / LENGHT)
-	    else:
-	        value = (LENGHT - x) * (pulseRange / LENGHT)
-	    print("Running " + str(value))
-	    pi.set_PWM_dutycycle(PIN, value)
-	    time.sleep(TIME)
+    print("Pulsing complete, switching off")
+    pi.write(4, 0)
 
 
+def testServo(PIN, MIN=850, CENTER=1350, MAX=1750):
 
-def testServo(SERVO, MIN=850, CENTER=1350, MAX=1750):
-    print("Centerig Steering Servo")
-    pi.set_servo_pulsewidth(SERVO, CENTER)
+    print("Commencing Servo Testing")
+    print("Centering Steering Servo")
+    pi.set_servo_pulsewidth(PIN, CENTER)
 
     time.sleep(3)
     print("To the left, to the left")
 
     for i in range(CENTER, MIN, -20):
-	    pi.set_servo_pulsewidth(SERVO, i)
+	    pi.set_servo_pulsewidth(PIN, i)
 	    time.sleep(0.1)
 
-    print("Hoorah")
+    print("Sir, yes sir")
     time.sleep(3)
     print("Bring it back now")
 
     for i in range(MIN, CENTER, 20):
-	    pi.set_servo_pulsewidth(SERVO, i)
+	    pi.set_servo_pulsewidth(PIN, i)
 	    time.sleep(0.01)
 
-    print("Hoorah")
+    print("Sir, yes sir")
     time.sleep(3)
-    print("Centerig Steering Servo")
+    print("To the right, to the right")
 
     for i in range (CENTER, MAX, 20):
-	    pi.set_servo_pulsewidth(SERVO, i)
+	    pi.set_servo_pulsewidth(PIN, i)
 	    time.sleep(0.1)
 
-    print("Hoorah")
+    print("Sir, yes sir")
     time.sleep(3)
-    print("Centerig Steering Servo")
+    print("Centering Steering Servo")
 
-    pi.set_servo_pulsewidth(SERVO, CENTER)
+    pi.set_servo_pulsewidth(PIN, CENTER)
     time.sleep(0.5)
-    pi.set_servo_pulsewidth(SERVO, 0)
+    pi.set_servo_pulsewidth(PIN, 0)
     print("Servo test complete")
 
 
@@ -71,15 +75,11 @@ GPIO  = 4
 SERVO = 7
 pi    = pigpio.pi()
 
-print("Testing Servo")
 testServo(SERVO)
 time.sleep(3)
+testPulse(GPIO)
 
-print("Pulsing")
-pulse(GPIO, 0.05, 30)
 print("All done")
-
-pi.write(4, 0)
 
 # Clear everything up and stop using the GPIO
 pi.stop()
