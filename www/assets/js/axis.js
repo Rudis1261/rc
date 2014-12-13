7f5d2eefed02e05181c0c40cad7dc2d8dee8b51a
Rudi Strydom <iam@thatguy.co.za>
2014-12-13 17:35:37 +0200

    Adding dedicated JS file

--- www/assets/js/axis.js
+++ www/assets/js/axis.js
@@ -1,12 +1,31 @@
 (function($){
 
     // Reset the env
-    var _startX         = 0;
-    var _startY         = 0;
-    var _endX           = 0;
-    var _endY           = 0;
-    var _dragging       = false;
-    var _trottleInput   = 20;
+    var _dragging      = false;
+    var _trottleInput  = 10;
+    var _axisX         = $("[data-type='drag-area'][data-axis='x']");
+    var _axisY         = $("[data-type='drag-area'][data-axis='y']");
+    var _parentOffsetX = _axisX.offset();
+    var _parentOffsetY = _axisY.offset();
+    var _startX        = Math.round(_axisX.width() / 2);
+    var _startY        = Math.round(_axisY.height() / 2);
+    var _endX          = _startX;
+    var _endY          = _startY;
+
+    window.setInterval(function(){_zero();}, 200);
+
+    var _zero = function() {
+        //console.log("TESTING");
+        if (_dragging == false && (_endX != 0 || _endY != 0)) {
+            _endY = 0;
+            _endX = 0;
+            console.log("ZEROING");
+        }
+
+        else if (_dragging) {
+
+        }
+    }
 
     // Get the coords regardless whether it's a mouse or touch event
     var pointerPositionXY = function(e){
@@ -17,13 +36,13 @@
         if (touchEvents.indexOf(e.type)){
             // Touch Type events
             var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-            out.x = touch.pageX;
-            out.y = touch.pageY;
+            out.x = touch.pageX - _parentOffsetX.left;
+            out.y = touch.pageY - _parentOffsetY.top;
 
         } else if (mouseEvents.indexOf(e.type)){
             // Mouse Type events
-            out.x = e.pageX;
-            out.y = e.pageY;
+            out.x = e.pageX - _parentOffsetX.left;
+            out.y = e.pageY - _parentOffsetY.top;
         }
         return out;
     };
@@ -41,7 +60,7 @@
     // Throttle the mouse moves somewhat, to ensure we do not flood the comms
     $("[data-type='drag-area']").on('touchmove mousemove', _.throttle(
         function(event){
-            _track(event);
+            _track(event, $(this));
         },_trottleInput)
     );
 
@@ -50,33 +69,53 @@
         event.preventDefault();
         var getXY   = pointerPositionXY(event);
         _dragging   = true;
-        _startX     = getXY.x;
-        _startY     = getXY.y;
     }
 
     // On mouse move / touch move, this tracks the position and sends the commands
-    function _track(event) {
+    function _track(event, element) {
         event.preventDefault();
         if (_dragging) {
 
             // Get the current position, and the distance we have since dragged
-            var getXY       = pointerPositionXY(event);
-            _endX           = getXY.x;
-            _endY           = getXY.y;
-            _getDistance    = _distance();
+            var getXY = pointerPositionXY(event);
+
+            if (element.data('axis') == 'x') {
+                _endX = getXY.x;
+            }
+
+            if (element.data('axis') == 'y') {
+                _endY = getXY.y;
+            }
+
+            _getDistance = _distance();
+            var xPercentile = Math.round(_getDistance.x / _startX * 100);
+            var yPercentile = Math.round(- _getDistance.y / _startY * 100);
+
+            xPercentile = (xPercentile > 100) ? 100 : xPercentile;
+            xPercentile = (xPercentile < -100) ? -100 : xPercentile;
+
+            yPercentile = (yPercentile > 100) ? 100 : yPercentile;
+            yPercentile = (yPercentile < -100) ? -100 : yPercentile;
+            console.log("DIST_Y: " + yPercentile + ", DIST_X: " + xPercentile);
+
+            $('.output').html("Y:" + yPercentile + ", X:" + xPercentile);
+
+            console.log(JSON.stringify({
+                action: "control",
+                x: xPercentile,
+                y: yPercentile
+            }));
+
 
             // Send the command via the BTH
-            bth.action(
+            /*bth.action(
                 JSON.stringify({
                     action: "mouse-move",
                     x: _getDistance.x,
                     y: _getDistance.y
                 })
-            );
-
-            // Set the start to the current positions. This ensures that movements are relative
-            _startX = _endX;
-            _startY = _endY;
+            );*/
+            //_.throttle(_track(event, element),_trottleInput);
         }
     }
 
